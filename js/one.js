266c6e5ec8e5c5c4c60d91905ecedbb6d9fba8d0
brettkanker <stanisic.luka44@gmail.com>
2023-04-05 10:19:17 +0200

    sketch 1 amplitude bar

--- js/one.js
+++ js/one.js
@@ -1,7 +1,43 @@
+var song;
+var amp;
+var button;
+
+var volhistory = [];
+
+function toggleSong() {
+    if (song.isPlaying()){
+        song.pause();
+    } else {
+        song.play();
+    }
+}
+
+function preload() {
+  song = loadSound("../assets/Yellow_Swans-Sovereign.mp3");
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1600, 800);
+  button = createButton('Pause');
+  button.mousePressed(toggleSong);
+  song.play();
+  amp = new p5.Amplitude();
 }
 
 function draw() {
-  background(220);
+  background(0);
+  var vol = amp.getLevel();
+  volhistory.push(vol);
+  stroke(255);
+  noFill();
+  beginShape();
+  for (var i = 0; i < volhistory.length; i++){
+    var y = map(volhistory[i], 0, 1, height/2, 0);
+    vertex(i, y);
+  }
+  endShape();
+
+  if (volhistory.length > width){
+    volhistory.splice(0, 1);
+  }
 }
