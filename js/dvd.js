7be13a22bb2b0ab9a91083d32adce481af83822b
brettkanker <stanisic.luka44@gmail.com>
2023-04-26 09:46:20 +0200

    Added agents category and dvd sketch

---
+++ js/dvd.js
@@ -0,0 +1,33 @@
+let x, y;
+let xspeed, yspeed;
+let dvd;
+
+function preload() {
+  dvd = loadImage('/assets/dvd_logo.png');
+}
+
+function setup() {
+  createCanvas(800, 600);
+  dvd.resize(100, 50);
+  x = random(width - dvd.width);
+  y = random(height - dvd.height);
+  xspeed = 3;
+  yspeed = 3;
+}
+
+function draw() {
+  background(0);
+  image(dvd, x, y);
+
+  x += xspeed * mouseX * 0.01;
+  y += yspeed * mouseY * 0.01;
+
+  if (x + dvd.width >= width || x <= 0) {
+    xspeed *= -1;
+    tint(random(255), random(255), random(255));
+  }
+  if (y + dvd.height >= height || y <= 0) {
+    yspeed *= -1;
+    tint(random(255), random(255), random(255));
+  }
+}
