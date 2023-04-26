18b15cd8213f501c6c57ebee3a1b344c9d1aa4c0
brettkanker <stanisic.luka44@gmail.com>
2023-04-26 11:40:37 +0200

    Changed direction sketch, added fails

--- js/directions.js
+++ js/directions.js
@@ -15,11 +15,12 @@ const NORTHWEST = 7;
 let positions = [];
 
 function setup() {
-  createCanvas(800, 600);
+  let canvas = createCanvas(800, 600);
+  canvas.style('border', '2px solid red');
   // Initialize position and direction randomly
   xPos = random(width);
   yPos = random(height);
-  xDir = random(-5, 5);
+  xDir = random(-5, 5); //floor/cealing/int
   yDir = random(-5, 5);
 }
 
@@ -30,29 +31,39 @@ function draw() {
   positions.push(createVector(xPos, yPos));
 
   // Draw the line using the positions array
-  stroke(0);
-  strokeWeight(2);
+  stroke(0, 0, 0, 100);
+  strokeWeight(4);
   noFill();
-  beginShape();
+//   beginShape();
+//   for (let i = 0; i < positions.length; i++) {
+//     vertex(positions[i].x, positions[i].y);
+//   }
+//   endShape();
   for (let i = 0; i < positions.length; i++) {
-    vertex(positions[i].x, positions[i].y);
+    point(positions[i].x, positions[i].y);
+    // ellipse(positions[i].x, positions[i].y, 50);
   }
-  endShape();
 
   // Move the shape based on direction
   xPos += xDir;
   yPos += yDir;
 
   // Handle edge cases
-  if (xPos > width || xPos < 0) {
-    xDir = -xDir;
+  if(xPos > width){
+    xPos = 0
   }
-  if (yPos > height || yPos < 0) {
-    yDir = -yDir;
+  else if(xPos < 0){
+    xPos = width
+  }
+  else if(yPos < 0){
+    yPos = height
+  }
+  else if(yPos > height){
+    yPos = 0
   }
 
   // Change direction randomly
-  if (random() < 0.05) {
+  if (random() < map(mouseX, 0, width, 0.10, 1)) {
     const dir = floor(random(8));
     switch (dir) {
       case NORTH:
