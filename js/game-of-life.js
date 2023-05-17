22e28f412609a3c8011002f1e3a6d50de920cf1d
brettkanker <stanisic.luka44@gmail.com>
2023-05-17 10:48:49 +0200

    Added more functions to game of life

--- js/game-of-life.js
+++ js/game-of-life.js
@@ -13,32 +13,35 @@ function make2DArray(cols, rows) {
     return arr;
   }
   
+  let button;
   let grid;
   let cols;
   let rows;
   let resolution = 10;
+  let isGenerating = false;
   
   function setup() {
-    createCanvas(600, 400);
+    createCanvas(1600, 1000);
+    button = createButton("Start");
+    button.mouseClicked(startGeneration);
     cols = width / resolution;
     rows = height / resolution;
-  
+
     grid = make2DArray(cols, rows);
     for (let i = 0; i < cols; i++) {
       for (let j = 0; j < rows; j++) {
-        grid[i][j] = floor(random(2));
+        grid[i][j] = 0;
       }
     }
   }
   
   function draw() {
     background(0);
-  
     for (let i = 0; i < cols; i++) {
       for (let j = 0; j < rows; j++) {
         let x = i * resolution;
         let y = j * resolution;
-        if (grid[i][j] == 1) {
+        if (grid[i][j] === 1) {
           fill(190, 34, 201);
           stroke(0);
           rect(x, y, resolution - 1, resolution - 1);
@@ -46,31 +49,29 @@ function make2DArray(cols, rows) {
       }
     }
   
-    let next = make2DArray(cols, rows);
+    if (isGenerating){
+        let next = make2DArray(cols, rows);
   
-    // Compute next based on grid
-    for (let i = 0; i < cols; i++) {
-      for (let j = 0; j < rows; j++) {
-        let state = grid[i][j];
-        // Count live neighbors!
-        let sum = 0;
-        let neighbors = countNeighbors(grid, i, j);
-  
-        if (state == 0 && neighbors == 3) {
-          next[i][j] = 1;
-        } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
-          next[i][j] = 0;
-        } else {
-          next[i][j] = state;
-        }
-  
-      }
+        // Compute next based on grid
+        for (let i = 0; i < cols; i++) {
+          for (let j = 0; j < rows; j++) {
+            let state = grid[i][j];
+            // Count live neighbors!
+            let sum = 0;
+            let neighbors = countNeighbors(grid, i, j);
+      
+            if (state == 0 && neighbors == 3) {
+              next[i][j] = 1;
+            } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
+              next[i][j] = 0;
+            } else {
+              next[i][j] = state;
+            }
+
+          }
+        } 
+        grid = next;
     }
-  
-    grid = next;
-  
-  
-  
   }
   
   
@@ -86,3 +87,34 @@ function make2DArray(cols, rows) {
     sum -= grid[x][y];
     return sum;
   }
+
+  // What I added
+
+  function startGeneration() {
+    isGenerating = true;
+  }
+
+  function mouseDragged() {
+    if (!isGenerating) {
+      // Get the cell indices based on the mouse position
+      let i = Math.floor(mouseX / resolution);
+      let j = Math.floor(mouseY / resolution);
+  
+      // Set the cell state to alive (1)
+      grid[i][j] = 1;
+    }
+  }  
+  
+  function mousePressed() {
+    // Get the cell indices based on the mouse position
+    let i = Math.floor(mouseX / resolution);
+    let j = Math.floor(mouseY / resolution);
+  
+    // Toggle the cell state between alive (1) and dead (0)
+    if (grid[i][j] === 1) {
+      grid[i][j] = 0;
+    } else {
+      grid[i][j] = 1;
+    }
+  }
+  
